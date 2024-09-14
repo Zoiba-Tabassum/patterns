cfd9da6e412552182946b921fe572cf62b3c093f
Zoiba Tabassum <zoibatabassum@gmail.com>
2024-09-14 23:43:13 +0500

    first

--- patterns.cpp
+++ patterns.cpp
@@ -2,28 +2,26 @@
 using namespace std;
 int main()
 {
-    int row;
-    cout << "Hey!! Playing around!!, must be fun *_* \n";
-    cin >> row;
-
-    for (int i = 0; i <= row; i++) {
-        for (int j = row; j < i; j--) {
-            cout << "- ";
-        }
-        for (int j = row; j >= 2 * (row - i); j--) {
-            cout << "* ";
-        }
-        cout << endl;
+    int row, column;
+ 
+  // Hollow rectangle
+  cout<<"Enter number of rows \t\t";
+  cin >> row;
+  cout<<"Enter number of columns \t";
+  cin>> column;
+  int v=column-1;
+  for (int i=0; i<row; i++){
+    for (int j=0; j<column ; j++){
+      if(j==0 || j==(column-1) || i==0 || i==(row-1) ){
+      cout<<"* "; 
+      }
+      else{
+        cout<<"  ";
+      }
+  
     }
-    for (int i = 0; i <= row; i++) {
-        for (int j = 0; j < i; j++) {
-            cout << "  ";
-        }
-        for (int j = 0; j <= 2*(row - i); j++) {
-            cout <<"* ";
-        }
+    cout<<endl;
+  }
+  
 
-        cout << endl;
-       
-    }
 }
